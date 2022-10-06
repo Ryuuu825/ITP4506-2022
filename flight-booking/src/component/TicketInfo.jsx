471de0dcf9e73cca62ac1174427bd865c9d1cff1
Ben POON <jyuban@outlook.com>
2022-10-07 02:04:56 +0800

    Comfortable design

--- flight-booking/src/component/TicketInfo.jsx
+++ flight-booking/src/component/TicketInfo.jsx
@@ -1,5 +1,5 @@
 import * as Logos from "./LogoPath";
-import { Info_SVG } from "./SVGPath";
+import { HKExpree_SVG } from "./SVGPath";
 
 export default function TicketInfo({airline}) {
 	return (
@@ -7,7 +7,7 @@ export default function TicketInfo({airline}) {
 			<div className="shadow-md flex flex-row w-full mb-2 border rounded-lg border-gray-200 bg-white">
 				<div className="flex flex-col flex-2 p-2 w-8/12">
 					<div className="flex flex-row items-center justify-center p-2 ">
-						<Info_SVG />
+						<HKExpree_SVG className="w-3"/>
 						<div className="flex flex-col p-2">
 							<p className="text-base font-bold">8:45 AM</p>
 							<p className="text-xs">HKG - Oct 8</p>
@@ -27,7 +27,7 @@ export default function TicketInfo({airline}) {
 				<div className="flex flex-col flex-1 p-4 items-end my-2 border-l">
 					<p className="text-xl font-bold">HKD4,511.74</p>
 					<p className="text-sm mb-4">Total price for all travelers</p>
-					<button className="w-full h-10 border-4 border-amber-400 rounded-sm font-bold bg-amber-400 text-gray-700 hover:border-orange-300 hover:bg-orange-300 focus:ring-4 focus:outline-none focus:ring-orange-200">View Deal</button>
+					<button className="w-full shadow-md h-10 border-4 border-amber-400 rounded-sm font-bold bg-amber-400 text-gray-700 hover:border-orange-300 hover:bg-orange-300 focus:ring-4 focus:outline-none focus:ring-orange-200">View Deal</button>
 				</div>
 			</div>
 		</div>
--- flight-booking/src/layout/DestinationBox.jsx
+++ flight-booking/src/layout/DestinationBox.jsx
@@ -20,7 +20,7 @@ export default function DestinationBox() {
 							<div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
 								<svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
 							</div>
-							<input type="text" id="search" className="bg-gray-50 border-4 border-orange-300 text-gray-900 text-sm focus:ring-4 focus:outline-none focus:ring-blue-300 block w-full pl-10 p-2.5" placeholder="Search Desctination" required="" />
+							<input type="text" id="search" className="bg-gray-50 border-4 border-orange-300 text-gray-900 text-sm focus:border-blue-300 focus:outline-none focus:ring-blue-300 block w-full pl-10 p-2.5" placeholder="Search Desctination" required="" />
 						</div>
 						<div className="flex">
 							<svg xmlns="http://www.w3.org/2000/svg"
@@ -40,7 +40,7 @@ export default function DestinationBox() {
 							<div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
 								<svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
 							</div>
-							<input type="date" id="search_date" className="bg-gray-50 border-4 border-orange-300 text-gray-900 text-sm focus:ring-4 focus:outline-none focus:ring-blue-300 block w-full pl-10 p-2.5" required="" />
+							<input type="date" id="search_date" className="bg-gray-50 border-4 border-orange-300 text-gray-900 text-sm focus:border-blue-300 focus:outline-none focus:ring-blue-300 block w-full pl-10 p-2.5" required="" />
 						</div>
 						<button type="submit" className="shadow-md rounded-lg inline-flex items-center py-2.5 px-3 text-sm font-medium text-white bg-blue-700 border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
 							<svg aria-hidden="true" className="-ml-1 w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg><p className="items-center w-full">Search</p></button>
