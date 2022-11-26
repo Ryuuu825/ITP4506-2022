f79f47af1f6be7388360b5c1b793904ae0fa830f
Ben POON <jyuban@outlook.com>
2022-11-26 16:46:39 +0800

    update seatform

--- flight-booking/src/pages/TranscationPage.jsx
+++ flight-booking/src/pages/TranscationPage.jsx
@@ -57,7 +57,8 @@ function TranscationBox({ data, passengers, setPassengers }) {
 	const [form, setForm] = useState(null);
 	const [formData, setFormData] = useState({
 		"passengers": [],
-		"seats": []
+		"seats": [],
+		"addOns": []
 	});
 
 	useEffect(() => {
@@ -324,7 +325,7 @@ function TopBox({ data }) {
 					<div className="flex flex-row items-center justify-center p-2 ">
 						<div className="flex flex-col p-2">
 							<p className="text-base font-bold"><Moment format="MMM DD">{data.date}</Moment></p>
-							<p className="text-lg text-yellow-500 font-bold">HKG - <Moment format="hh:mm A" date={fullOutTime} /></p>
+							<p className="text-lg text-yellow-400 font-bold">HKG - <Moment format="hh:mm A" date={fullOutTime} /></p>
 							<p className="text-base font-bold">Hong Kong</p>
 						</div>
 						<div className="flex flex-col flex-grow p-2 items-center">
@@ -353,7 +354,7 @@ function TopBox({ data }) {
 						</div>
 						<div className="flex flex-col p-2">
 							<p className="text-base font-bold"><Moment format="MMM DD">{data.ticket.arrivalTime}</Moment></p>
-							<p className="text-lg text-yellow-500 font-bold">{data.dest} - <Moment format="hh:mm A" date={data.ticket.arrivalTime} /></p>
+							<p className="text-lg text-yellow-400 font-bold">{data.dest} - <Moment format="hh:mm A" date={data.ticket.arrivalTime} /></p>
 							<p className="text-base font-bold">{(airports.filter(d => d.code == data.dest)[0]).area.split(",")[0]}</p>
 						</div>
 					</div>
@@ -387,7 +388,7 @@ function TopBox({ data }) {
 	C217.214,15.205,215.407,10.016,211.744,6.089z"/>
 											</svg>
 											<p className="text-sm grow">Seat selection</p>
-											<p className="text-sm ml-4 text-right text-yellow-500 font-bold w-48">Complimentary (Standard & Forward Zone Seats)</p>
+											<p className="text-sm ml-4 text-right text-yellow-400 font-bold w-48">Complimentary (Standard & Forward Zone Seats)</p>
 										</div>
 										<div className="flex flex-row items-center">
 											<svg version="1.1" fill="currentColor" className="mr-4 my-1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
@@ -423,7 +424,7 @@ function TopBox({ data }) {
 										<div className="flex flex-row items-center">
 											<svg xmlns="http://www.w3.org/2000/svg" className="mr-4 my-1 feather feather-refresh-ccw" width="30px" height="30px" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polyline points="1 4 1 10 7 10"></polyline><polyline points="23 20 23 14 17 14"></polyline><path d="M20.49 9A9 9 0 0 0 5.64 5.64L1 10m22 4l-4.64 4.36A9 9 0 0 1 3.51 15"></path></svg>
 											<p className="text-sm grow">Booking change fee<br />(a fare difference may apply)</p>
-											<p className="text-sm ml-4 text-right text-yellow-500 font-bold w-48">Complimentary</p>
+											<p className="text-sm ml-4 text-right text-yellow-400 font-bold w-48">Complimentary</p>
 										</div>
 										<div className="flex flex-row items-center">
 											<svg width="30px" height="30px" fill="currentColor" className="mr-4 my-1" viewBox="0 0 64 64" style={{ "stroke": "#000", "strokeLinecap": "round", "strokeLinejoin": "round", "strokeWidth": "2px" }} xmlns="http://www.w3.org/2000/svg"><defs>
@@ -437,7 +438,7 @@ function TopBox({ data }) {
 						</div> : null}
 				</div>
 				<div className="flex flex-col flex-1 pl-4 my-2 border-l">
-					<div className="w-full flex justify-end items-center flex-row font-bold text-sm text-yellow-500">
+					<div className="w-full flex justify-end items-center flex-row font-bold text-sm text-yellow-400">
 						<label className="hover:underline cursor-pointer" onClick={showDetailHandler}>{showDetail ? "Hide" : "View"} details</label>
 						<svg className={showDetail ? "rotate-180 feather feather-chevron-down" : "feather feather-chevron-down"} xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polyline points="6 9 12 15 18 9"></polyline></svg>
 					</div>
